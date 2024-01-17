9e19970d3cd4fc86cbc2e923581ef812dc3364c3
don-galak <ptagalac@gmail.com>
2024-01-17 22:29:34 +0200

    jlox: generateAst - improve python version

--- java/com/craftinginterpreters/tool/generateAst.py
+++ java/com/craftinginterpreters/tool/generateAst.py
@@ -1,4 +1,39 @@
 import sys
 
-n = len(sys.argv)
-print("total args passed", n)
+try:
+    path = sys.argv[1]
+except:
+    print("Usage: generate_ast <output directory>", sys.argv)
+    exit(1)
+
+types = [
+  "Binary   :  Expr left, Token operator, Expr right",
+  "Grouping : Expr expression",
+  "Literal  : Object value",
+  "Unary    : Token operator, Expr right",
+]
+
+def defineVisitor(file, baseName, types):
+    file.write("  interface Visitor<R> {")
+
+def defineAst(outputDir, baseName, types):
+    path = outputDir + "/" + baseName + ".java"
+    file = open(path, "w")
+
+    file.write("package com.craftinginterpreters.lox;\n\n")
+    file.write("import java.util.List;\n\n")
+    file.write("abstract class " + baseName + " {\n")
+
+    defineVisitor(file, baseName, types)
+
+    # The AST classes.
+
+    # The base accept() method.
+    file.write("\n")
+    file.write("  abstract <R> R accept(Visitor<R> visitor);\n")
+    file.write("}")
+    file.close()
+
+
+
+defineAst(path, "Expr", types)
