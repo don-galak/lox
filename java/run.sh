a05b9a421d2b9bc671fded63b4ea20417630d435
don-galak <ptagalac@gmail.com>
2024-01-28 19:17:05 +0200

    jlox: update utils

--- java/run.sh
+++ java/run.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-file=$1
-
 javac com/craftinginterpreters/lox/Lox.java
 
-if [ -z "$1" ]
-    then
-        java com/craftinginterpreters/lox/Lox    
-    else
-        java com/craftinginterpreters/lox/Lox "./examples/$file"
-fi
+printf "Select one of the existing files to run. Type any other key, to run the repl.\n\nExit by typing: Ctrl+D.\n\n"
+
+select file in "./examples"/*
+do 
+    java com/craftinginterpreters/lox/Lox $file
+done
+
