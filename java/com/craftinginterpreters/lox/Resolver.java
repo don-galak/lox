b4ce1d8d75ca5a13263809aff3cdba9d520e3674
don-galak <ptagalac@gmail.com>
2024-02-08 08:01:14 +0200

    jlox: get expressions

--- java/com/craftinginterpreters/lox/Resolver.java
+++ java/com/craftinginterpreters/lox/Resolver.java
@@ -149,6 +149,12 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Void visitGetExpr(Expr.Get expr) {
+        resolve(expr.object);
+        return null;
+    }
+
+    @Override
     public Void visitVariableExpr(Expr.Variable expr) {
         if (!scopes.isEmpty() && scopes.peek().get(expr.name.lexeme) == Boolean.FALSE) {
             Lox.error(expr.name, "Can't read local variable in its own initializer.");
