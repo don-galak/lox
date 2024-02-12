b94efd1d274dd96e9a173f52d08bdd851cbe705d
don-galak <ptagalac@gmail.com>
2024-02-12 22:08:03 +0200

    jlox: add superclass and subclass support

--- java/com/craftinginterpreters/tool/generateAst.js
+++ java/com/craftinginterpreters/tool/generateAst.js
@@ -21,7 +21,7 @@ defineAst(outputDir, "Expr", [
 ]);
 defineAst(outputDir, "Stmt", [
   "Block      : List<Stmt> statements",
-  "Class      : Token name, List<Stmt.Function> methods",
+  "Class      : Token name, Expr.Variable superclass, List<Stmt.Function> methods",
   "Expression : Expr expression",
   "Function   : Token name, List<Token> params, List<Stmt> body",
   "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
