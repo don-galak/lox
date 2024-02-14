a1dae58dd9ba4d2968037b4dc32aabcc3a9cf837
don-galak <ptagalac@gmail.com>
2024-02-14 19:48:00 +0200

    move examples in root

--- java/run.sh
+++ java/run.sh
@@ -4,8 +4,7 @@ javac com/craftinginterpreters/lox/Lox.java
 
 printf "Select one of the existing files to run. Type any other key, to run the repl.\n\nExit by typing: Ctrl+D.\n\n"
 
-select file in "./examples"/*
+select file in "../examples"/*
 do 
     java com/craftinginterpreters/lox/Lox $file
 done
-
