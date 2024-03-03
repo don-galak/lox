a426747a6473c878b7ed4d814c561f1e32d8bf29
don-galak <ptagalac@gmail.com>
2024-03-03 20:20:33 +0200

    clox: add scanner skeleton

--- clox/main.c
+++ clox/main.c
@@ -25,10 +25,10 @@ static void repl()
 }
 
 /**
- * We want to allocate a big enough string to read the whole file, but we don't 
+ * We want to allocate a big enough string to read the whole file, but we don't
  * know how big the file is until we have read it.
  * 1. We open the file ("rb" means the file is in binary. Not sure why it's needed)
- * 2. We call fseek with 0l (long integer value with all the bits set to zero - generally the definition of 0) 
+ * 2. We call fseek with 0l (long integer value with all the bits set to zero - generally the definition of 0)
  *    and SEEK_END to reach the end of the file.
  * 3. Then we call ftell, which tells us how many bytes we are from the start of the file.
  *    Since we seeked to the end, that's the size.
@@ -36,9 +36,11 @@ static void repl()
  * 5. Allocate a string of fileSize.
  * 6. Read the whole file in a single batch.
  */
-static char* readFile(const char* path) {
-    FILE* file = fopen(path, "rb");
-    if (file == NULL) {
+static char *readFile(const char *path)
+{
+    FILE *file = fopen(path, "rb");
+    if (file == NULL)
+    {
         fprintf(stderr, "Could not open file \"%s\".\n", path);
         exit(74);
     }
@@ -47,14 +49,16 @@ static char* readFile(const char* path) {
     size_t fileSize = ftell(file);
     rewind(file);
 
-    char* buffer = (char*)malloc(fileSize + 1);
-    if (buffer == NULL) {
+    char *buffer = (char *)malloc(fileSize + 1);
+    if (buffer == NULL)
+    {
         fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
         exit(74);
     }
 
     size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
-    if (bytesRead < fileSize) {
+    if (bytesRead < fileSize)
+    {
         fprintf(stderr, "Could not read file \"%s\".\n", path);
     }
 
