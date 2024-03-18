61f813c0b4a3dafb2f4099587708db9dd99ced4f
don-galak <ptagalac@gmail.com>
2024-03-18 10:15:07 +0200

    clox: handling syntax errors - part 1

--- clox/compiler.c
+++ clox/compiler.c
@@ -8,16 +8,50 @@ typedef struct
 {
     Token current;
     Token previous;
+    bool hadError;
 } Parser;
 
 Parser parser;
+
+static void errorAt(Token* token, const char* message)
+{
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    }
+    else if (token->type == TOKEN_ERROR) {
+        // Nothing.
+    }
+    else {
+        /**
+         * '%.*s' is a format specifier for a string.
+         * The '.*' indicates that the width of the string will be determined dynamically
+         * by the argument that follows it. In this case, 'token.length' determines the width of the string.
+         * 'token.start' is the pointer to the start of the string.
+         */
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+static void error(const char* message)
+{
+    errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char* message)
+{
+    errorAt(&parser.current, message);
+}
 
 static void advance()
 {
     parser.previous = parser.current;
 
-    for (;;)
-    {
+    for (;;) {
         parser.current = scanToken();
         if (parser.current.type != TOKEN_ERROR)
             break;
@@ -32,10 +66,11 @@ static void advance()
  * After the expression is compiled we should be at the end
  * of the source code, so we check for the EOF token.
  */
-bool compile(const char *source, Chunk *chunk)
+bool compile(const char* source, Chunk* chunk)
 {
     initScanner(source);
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression");
+    return !parser.hadError;
 }
