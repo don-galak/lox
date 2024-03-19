48a5246dce11afb748db0ad5b3c754f305ead3fc
don-galak <ptagalac@gmail.com>
2024-03-19 20:54:54 +0200

    clox: emitting bytecode

--- clox/compiler.c
+++ clox/compiler.c
@@ -13,6 +13,11 @@ typedef struct {
 } Parser;
 
 Parser parser;
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+    return compilingChunk;
+}
 
 static void errorAt(Token* token, const char* message) {
     if (parser.panicMode) return;
@@ -66,6 +71,23 @@ static void consume(TokenType type, const char* message) {
     errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte) {
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn() {
+    emitByte(OP_RETURN);
+}
+
+static void endCompiler() {
+    emitReturn();
+}
+
 /**
  * The call to advance() "primes the pump" on the scanner.
  * Then a single expression is parsed.
@@ -74,6 +96,7 @@ static void consume(TokenType type, const char* message) {
  */
 bool compile(const char* source, Chunk* chunk) {
     initScanner(source);
+    compilingChunk = chunk;
 
     parser.hadError = false;
     parser.panicMode = false;
@@ -81,5 +104,6 @@ bool compile(const char* source, Chunk* chunk) {
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression");
+    endCompiler();
     return !parser.hadError;
 }
