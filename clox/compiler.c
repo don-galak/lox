ecbaa0531fedbefebc3c291ac85b617814a6da06
don-galak <ptagalac@gmail.com>
2024-03-20 18:12:42 +0200

    clox: [minor] format

--- clox/compiler.c
+++ clox/compiler.c
@@ -13,16 +13,16 @@ typedef struct {
 } Parser;
 
 typedef enum {
-    PREC_ONE,
-    PREC_ASSIGNMENT, // = 
-    PREC_OR, // or
-    PREC_AND, // AND
-    PREC_EQUALITY, // == !=
-    PREC_COMPARISON, // < > <= >=
-    PREC_TERM,  // + -
-    PREC_FACTOR, // * /
-    PREC_UNARY, // ! -
-    PREC_CALL, // . ()
+    PREC_NONE,
+    PREC_ASSIGNMENT,  // =
+    PREC_OR,          // or
+    PREC_AND,         // and
+    PREC_EQUALITY,    // == !=
+    PREC_COMPARISON,  // < > <= >=
+    PREC_TERM,        // + -
+    PREC_FACTOR,      // * /
+    PREC_UNARY,       // ! -
+    PREC_CALL,        // . ()
     PREC_PRIMARY
 } Precedence;
 
