237aa4e7babccb10a7f7555568c035f6b80f779c
don-galak <ptagalac@gmail.com>
2024-04-13 12:40:02 +0300

    clox: fix bug

--- clox/object.h
+++ clox/object.h
@@ -4,12 +4,12 @@
 #include "common.h"
 #include "value.h"
 
-#define OBJ_TYPE(value)     (AS_OBJ(value)->type)
+#define OBJ_TYPE(value)        (AS_OBJ(value)->type)
 
-#define IS_STRING(value)    isObjType(value, OBJ_STRING)
+#define IS_STRING(value)       isObjType(value, OBJ_STRING)
 
-#define AS_STRING(value)    ((ObjString*)AS_OBJ(value))
-#define AS_CSTRING(value)   (((ObjString*)AS_OBJ(value))->chars)
+#define AS_STRING(value)       ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
     OBJ_STRING,
