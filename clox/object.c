afdf368dfe37114c1a355b231bdf21807cac8e49
don-galak <ptagalac@gmail.com>
2024-04-14 21:37:23 +0300

    clox: hashing strings

--- clox/object.c
+++ clox/object.c
@@ -18,15 +18,27 @@ static Obj* allocateObject(size_t size, ObjType type) {
     return object;
 }
 
-static ObjString* allocateString(char* chars, int length) {
+static ObjString* allocateString(char* chars, int length, uint32_t hash) {
     ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
     string->length = length;
     string->chars = chars;
+    string->hash = hash;
     return string;
 }
 
+// FNV-1a hashing algorithm
+static uint32_t hashString(const char* key, int length) {
+    uint32_t hash = 2166136261u;
+    for (int i = 0; i < length; i++) {
+        hash ^= (uint8_t)key[i];
+        hash *= 16777619;
+    }
+    return hash;
+}
+
 ObjString* takeString(char* chars, int length) {
-    return allocateString(chars, length);
+    uint32_t hash = hashString(chars, length);
+    return allocateString(chars, length, hash);
 }
 
 /**
@@ -34,10 +46,11 @@ ObjString* takeString(char* chars, int length) {
  * This way we can pass the character array to C standard library functions that expect a terminated string.
  */
 ObjString* copyString(const char* chars, int length) {
+    uint32_t hash = hashString(chars, length);
     char* heapChars = ALLOCATE(char, length + 1);
     memcpy(heapChars, chars, length);
     heapChars[length] = '\0';
-    return allocateString(heapChars, length);
+    return allocateString(heapChars, length, hash);
 }
 
 void printObject(Value value) {
