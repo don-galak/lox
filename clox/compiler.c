b4ce2e0a8f344fa6cd424029fb78468d42ce5639
don-galak <ptagalac@gmail.com>
2024-04-30 17:15:09 +0300

    clox: for loop - increment clause

--- clox/compiler.c
+++ clox/compiler.c
@@ -544,7 +544,17 @@ static void forStatement() {
         emitJump(OP_POP); // Condition.
     }
 
-    consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+    if (!match(TOKEN_RIGHT_PAREN)) {
+        int bodyJump = emitJump(OP_JUMP);
+        int incrementStart = currentChunk()->count;
+        expression();
+        emitByte(OP_POP);
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        emitLoop(loopStart);
+        loopStart = incrementStart;
+        patchJump(bodyJump);
+    }
 
     statement();
     emitLoop(loopStart);
