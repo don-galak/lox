6c978842211678eee29749d9858aea55ae2429c9
don-galak <ptagalac@gmail.com>
2024-05-10 23:31:30 +0300

    clox: add tmp clox run

---
+++ clox/run.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+javac com/craftinginterpreters/lox/Lox.java
+
+printf "Select one of the existing files to run. Type any other key, to run the repl.\n\nExit by typing: Ctrl+D.\n\n"
+
+select file in "../examples"/*
+do
+    ./main $file
+done
