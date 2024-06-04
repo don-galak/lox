9fac29000a1941754ba7f5c82178e5b2c02b5590
Panagiotis Tagkalakis <43350605+don-galak@users.noreply.github.com>
2024-06-04 17:11:42 +0000

    clox: sweeping unused objects

--- clox/memory.c
+++ clox/memory.c
@@ -136,7 +136,7 @@ static void markRoots() {
     for (ObjUpvalue* upvalue = vm.openUpvalues;
          upvalue != NULL;
          upvalue = upvalue->next) {
-      markObject((Obj*)upvalue)
+      markObject((Obj*)upvalue);
     }
 
     markTable(&vm.globals);
@@ -150,13 +150,36 @@ static void traceReferences() {
   }
 }
 
+static void sweep() {
+  Obj* previous = NULL;
+  Obj* object = vm.objects;
+  while (object != NULL) {
+    if (object->isMarked) {
+      object->isMarked = false;
+      previous = object;
+      object = object->next;
+    } else {
+      Obj* unreached = object;
+      object = object->next;
+      if (previous != NULL) {
+        previous->next = object;
+      } else {
+        vm.objects = object;
+      }
+
+      freeObject(unreached);
+    }
+  }
+}
+
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
     printf("-- gc begin\n");
 #endif
 
-markRoots();
-traceReferences();
+  markRoots();
+  traceReferences();
+  sweep();
 
 #ifdef DEBUG_LOG_GC
     printf("-- gc end\n");
