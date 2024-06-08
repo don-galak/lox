0dbd53be6abaccfbf4bb8f8433ab9e09ae451bb3
Panagiotis Tagkalakis <43350605+don-galak@users.noreply.github.com>
2024-06-08 09:56:47 +0300

    clox: class objects

--- clox/object.c
+++ clox/object.c
@@ -25,6 +25,12 @@ static Obj* allocateObject(size_t size, ObjType type) {
     return object;
 }
 
+ObjClass* newClass(ObjString* name) {
+  ObjClass* klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
+  klass->name = name;
+  return klass;
+}
+
 ObjClosure* newClosure(ObjFunction* function) {
     ObjUpvalue** upvalues = ALLOCATE(ObjUpvalue*,
                                      function->upvalueCount);
@@ -122,21 +128,24 @@ static void printFunction(ObjFunction* function) {
 
 void printObject(Value value) {
     switch (OBJ_TYPE(value)) {
+        case OBJ_CLASS:
+          printf("%s", AS_CLASS(value)->name->chars);
+          break;
         case OBJ_CLOSURE:
-            printFunction(AS_CLOSURE(value)->function);
-            break;
+          printFunction(AS_CLOSURE(value)->function);
+          break;
         case OBJ_FUNCTION:
-            printFunction(AS_FUNCTION(value));
-            break;
+          printFunction(AS_FUNCTION(value));
+          break;
         case OBJ_NATIVE: {
-            printf("<native fn>");
-            break;
+          printf("<native fn>");
+          break;
         }
         case OBJ_STRING:
-            printf("%s", AS_CSTRING(value));
-            break;
+          printf("%s", AS_CSTRING(value));
+          break;
         case OBJ_UPVALUE:
-            printf("upvalue");
-            break;
+          printf("upvalue");
+          break;
     }
 }
