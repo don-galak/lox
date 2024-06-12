5c4ca32f032b69952a445cb30a61a1cd1d0db270
Panagiotis Tagkalakis <43350605+don-galak@users.noreply.github.com>
2024-06-12 20:36:53 +0300

    clox: format

--- clox/compiler.c
+++ clox/compiler.c
@@ -12,43 +12,43 @@
 #endif
 
 typedef struct {
-    Token current;
-    Token previous;
-    bool hadError;
-    bool panicMode;
+  Token current;
+  Token previous;
+  bool hadError;
+  bool panicMode;
 } Parser;
 
 typedef enum {
-    PREC_NONE,
-    PREC_ASSIGNMENT,  // =
-    PREC_OR,          // or
-    PREC_AND,         // and
-    PREC_EQUALITY,    // == !=
-    PREC_COMPARISON,  // < > <= >=
-    PREC_TERM,        // + -
-    PREC_FACTOR,      // * /
-    PREC_UNARY,       // ! -
-    PREC_CALL,        // . ()
-    PREC_PRIMARY
+  PREC_NONE,
+  PREC_ASSIGNMENT,  // =
+  PREC_OR,          // or
+  PREC_AND,         // and
+  PREC_EQUALITY,    // == !=
+  PREC_COMPARISON,  // < > <= >=
+  PREC_TERM,        // + -
+  PREC_FACTOR,      // * /
+  PREC_UNARY,       // ! -
+  PREC_CALL,        // . ()
+  PREC_PRIMARY
 } Precedence;
 
 typedef void (*ParseFn)(bool canAssign);
 
 typedef struct {
-    ParseFn prefix;
-    ParseFn infix;
-    Precedence precedence;
+  ParseFn prefix;
+  ParseFn infix;
+  Precedence precedence;
 } ParseRule;
 
 typedef struct {
-    Token name;
-    int depth;
-    bool isCaptured;
+  Token name;
+  int depth;
+  bool isCaptured;
 } Local;
 
 typedef struct {
-    uint8_t index;
-    bool isLocal;
+  uint8_t index;
+  bool isLocal;
 } Upvalue;
 
 /**
@@ -57,21 +57,21 @@ typedef struct {
  * TODO placeholder explain
  */
 typedef enum {
-    TYPE_FUNCTION,
-    TYPE_METHOD,
-    TYPE_SCRIPT,
+  TYPE_FUNCTION,
+  TYPE_METHOD,
+  TYPE_SCRIPT,
 } FunctionType;
 
 typedef struct Compiler {
-    struct Compiler* enclosing;
-    ObjFunction* function;
-    FunctionType type;
+  struct Compiler* enclosing;
+  ObjFunction* function;
+  FunctionType type;
 
     // clox allows up to 256 local variable declarations.
-    Local locals[UINT8_COUNT];
-    int localCount;
-    Upvalue upvalues[UINT8_COUNT];
-    int scopeDepth;
+  Local locals[UINT8_COUNT];
+  int localCount;
+  Upvalue upvalues[UINT8_COUNT];
+  int scopeDepth;
 } Compiler;
 
 typedef struct ClassCompiler {
