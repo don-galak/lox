69302700f3149ac6d0f8956df76b081ed6d0c856
Panagiotis Tagkalakis <43350605+don-galak@users.noreply.github.com>
2024-06-13 20:17:20 +0300

    clox: executing super accesses

--- clox/vm.c
+++ clox/vm.c
@@ -365,7 +365,7 @@ static InterpretResult run() {
                 if (!bindMethod(instance->klass, name)) {
                     return INTERPRET_RUNTIME_ERROR;
                 }
-        break;
+                break;
             }
             case OP_SET_PROPERTY: {
                 if (!IS_INSTANCE(peek(1))) {
@@ -380,6 +380,16 @@ static InterpretResult run() {
                 push(value);
                 break;
             }
+            case OP_GET_SUPER: {
+                ObjString* name = READ_STRING();
+                ObjClass* superclass = AS_CLASS(pop());
+
+                if (!bindMethod(superclass, name)) {
+                return INTERPRET_RUNTIME_ERROR;
+                }
+                break;
+            }
+
             case OP_EQUAL: {
                 Value b = pop();
                 Value a = pop();
